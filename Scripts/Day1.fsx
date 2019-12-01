d3ebdce0cb291fe87b0c943baf44b95992331821
Justin <jbudbo@gmail.com>
2019-12-01 07:37:05 -0500

    Day1 Parts 1 and 2

---
+++ Scripts/Day1.fsx
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+let readData (path:string) = File.ReadLines(path)
+let calculateFuel (mass:int) = mass / 3 - 2
+let rec calculateFuelDeep (mass:int) =
+    let start = calculateFuel mass
+    if start > 0 then
+        start + (calculateFuelDeep start)
+    else 
+        0
+
+let part1 (dataFile:string) =
+    let massage (x:string) = int x |> calculateFuel
+    readData dataFile
+        |> Seq.sumBy massage
+
+let part2 (dataFile:string) =
+    let massage (x:string) = int x |> calculateFuelDeep
+    readData dataFile
+    |> Seq.sumBy massage
