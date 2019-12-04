c32dd8f94775094f4256aa551d66495bb551614c
root <root@J300241-W102.corp.allscripts.com>
2019-12-04 10:22:43 -0500

    Day 4 part 1

--- Scripts/Day4.fsx
+++ Scripts/Day4.fsx
@@ -1,15 +1,26 @@
 let candidates = [|387638..919123|]
 
-let rec intToInts i = 
-    let work = seq {
-        yield i % 10
-        
-        let next = i / 10
+let intToInts i = 
+    // I think this could be done MUCH better but this splits the ints and keeps them in order
+    [|
+        i / 100000;
+        i % 100000 / 10000; 
+        i % 10000 / 1000; 
+        i % 1000 / 100;
+        i % 100 / 10;
+        i % 10;
+    |]
 
-        if next > 0 then yield! intToInts next
-    }
-    Seq.toArray work
+let qualifies (ints:int[]) :bool =
+    let windows = Array.windowed 2 ints
+
+    let hasPair (a:int[][]) :bool = a |> Array.exists (fun [|h;t|] -> h = t)
+    let incremental (a:int[][]) :bool = a |> Array.forall (fun [|h;t|] -> h <= t)
+
+    // Not a fan of this, seems we could/should curry
+    incremental windows && hasPair windows
 
 candidates
 |> Array.map intToInts
-|> Array.reduce
+|> Array.where qualifies
+|> Array.length
