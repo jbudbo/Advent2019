061094b35929b5ccf641b9ed613ca25b657a1f5f
Justin <jbudbo@gmail.com>
2019-12-07 07:27:11 -0500

    Day 2 Part 1 done

--- Scripts/Day2.fsx
+++ Scripts/Day2.fsx
@@ -3,18 +3,35 @@ open System.IO
 let readData (path:string) =  
     File.ReadAllText(path).Split ',' 
     |> Array.map int
-    |> Array.toList
 
 let part1 (dataFile:string) =
     let cleanData = readData dataFile
 
-    let execOp l =
-        match l with
-        | 1::[x;y;i] -> printfn "Add %i + %i in %i" x y i// cleanData.[i] = cleanData.[x] + cleanData.[y]
-        | 2::[x;y;i] -> printfn "Mult %i + %i in %i" x y i //cleanData.[i] = cleanData.[x] * cleanData.[y]
-        | 99::_ -> printfn "STOP"
-        | _ -> printfn "ERR"
+    //  Start puzzle
+    cleanData.[1] <- 12
+    cleanData.[2] <- 2
 
-    cleanData
-    |> List.chunkBySize 4
-    |> List.iter (fun x -> execOp x)
+    let add x y i =
+        cleanData.[i] <- cleanData.[x] + cleanData.[y]
+
+    let mult x y i =
+        cleanData.[i] <- cleanData.[x] * cleanData.[y]
+
+    let execOp (l:int[]) =
+        match Array.head l with
+        | 1 -> Some(add l.[1] l.[2] l.[3])
+        | 2 -> Some(mult l.[1] l.[2] l.[3])
+        | 99 -> None
+        | x -> failwithf "Unknown OP Code %i" x
+
+    let rec chunki i =
+        if i <= (cleanData.Length - 4) then
+            match Array.sub cleanData i 4 |> execOp with
+            | Some(_) -> chunki (i + 4)
+            | None -> printf "DONE"
+        
+    chunki 0
+
+    cleanData.[0]
+
+part1 "inputs/Day2"
